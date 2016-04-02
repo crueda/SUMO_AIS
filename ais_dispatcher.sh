8aaa8f28b28169226ba0ffc26f5719cfe18f968e
Carlos Rueda Morales <crueda@gmail.com>
2016-04-02 07:34:01 +0200

    new version by iñaky

--- ais_dispatcher.sh
+++ ais_dispatcher.sh
@@ -6,10 +6,10 @@
 # mail: carlos.rueda@deimos-space.com
 # version: 1.0
 
-##################################################################################
+########################################################################
 # version 1.0 release notes:
 # Initial version
-##################################################################################
+########################################################################
 
 from __future__ import division
 import time
@@ -23,6 +23,7 @@ import json
 import httplib2
 from threading import Thread
 import pika
+import MySQLdb
 
 
 ########################################################################
@@ -32,6 +33,11 @@ config = ConfigObj('./ais_dispatcher.properties')
 
 LOG = config['directory_logs'] + "/ais_dispatcher.log"
 LOG_FOR_ROTATE = 10
+
+DB_IP = "localhost"
+DB_NAME = "sumo"
+DB_USER = "root"
+DB_PASSWORD = "dat1234"
 
 RABBITMQ_HOST = config['rabbitMQ_HOST']
 RABBITMQ_PORT = config['rabbitMQ_PORT']
@@ -45,7 +51,6 @@ KCS_PORT = config['KCS_PORT']
 DEFAULT_SLEEP_TIME = float(config['sleep_time'])
 
 PID = "/var/run/ais_dispatcher"
-
 ########################################################################
 
 # Se definen los logs internos que usaremos para comprobar errores
@@ -87,127 +92,160 @@ pidfile.close()
 
 ########################################################################
 
-########################################################################
-# Definicion de funciones
-#
-########################################################################
+#socketKCS = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#connectedKCS = False
+#connectedQUEUE = False
+#connectionRetry = 5
 
-socketKCS = None
-SLEEP_TIME = DEFAULT_SLEEP_TIME
+def checkBoat(vehicleLicense):
+	try:
+		dbConnection = MySQLdb.connect(DB_IP, DB_USER, DB_PASSWORD, DB_NAME)
+	except Exception, error:
+		logger.error('Error connecting to database: IP:%s, USER:%s, PASSWORD:%s, DB:%s: %s', DB_IP, DB_USER, DB_PASSWORD, DB_NAME, error)
+	try:
+		cursor = dbConnection.cursor()
+		cursor.execute(""" SELECT DEVICE_ID from VEHICLE where VEHICLE_LICENSE= '%s' limit 0,1""" % (vehicleLicense,))
+		result = cursor.fetchall()
+		if len(result)==1 :
+			return result[0][0]
+		else :
+			return '0'
+		cursor.close
+		dbConnection.close
+	except Exception, error:
+		logger.error('Error executing query: %s', error)
 
-def connectKCS():
-    global socketKCS
+def addBoat(vehicleLicense):
+	try:
+		dbConnection = MySQLdb.connect(DB_IP, DB_USER, DB_PASSWORD, DB_NAME)
+	except Exception, error:
+		logger.error('Error connecting to database: IP:%s, USER:%s, PASSWORD:%s, DB:%s: %s', DB_IP, DB_USER, DB_PASSWORD, DB_NAME, error)
+	try:
+		query = """INSERT INTO VEHICLE (VEHICLE_LICENSE,BASTIDOR,ALIAS,POWER_SWITCH,ALARM_STATE,SPEAKER,START_STATE,WARNER,PRIVATE_MODE,WORKING_SCHEDULE,ALARM_ACTIVATED,PASSWORD,CELL_ID,ICON_DEVICE, KIND_DEVICE,AIS_TYPE,MAX_SPEED,CONSUMPTION,CLAXON,MODEL_TRANSPORT,PROTOCOL_ID,BUILT,CALLSIGN,MAX_PERSONS,MOB,EXCLUSION_ZONE,FLAG) VALUES (xxx,'',xxx,-1,-1,-1,'UNKNOWN',-1,0,0,0,'',0,1000,1,3,500,0.0,-1,'boat',0,0,xxx,-1,-1,0,'')"""
+		QUERY = query.replace('xxx', vehicleLicense)
+		cursor = dbConnection.cursor()
+		cursor.execute(QUERY)
+		dbConnection.commit()
+		logger.info('Boat %s added at database', vehicleLicense)
+		cursor.close
+		cursor = dbConnection.cursor()
+		cursor.execute("""SELECT LAST_INSERT_ID()""")
+		result = cursor.fetchall()
+		cursor.close
+		dbConnection.close()
+		logger.info('Boat added with DEVICE_ID: %s', result[0][0])
+        	return result[0][0]
+	except Exception, error:
+		logger.error('Error executing query : %s', error)
 
-    # conexion a KCS
-    socketKCS = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socketKCS.settimeout(0.1)
-    try:
-        socketKCS.connect((KCS_HOST, int(KCS_PORT)))
-        logger.info('Connected to KCS')
-        socketKCS.settimeout(0.05)
-    except Exception, error:
-        logger.error('Error connecting to KCS: %s', error)
+def addComplementary(vehicleLicense, deviceID):
+	try:
+		dbConnection = MySQLdb.connect(DB_IP, DB_USER, DB_PASSWORD, DB_NAME)
+	except Exception, error:
+		logger.error('Error connecting to database: IP:%s, USER:%s, PASSWORD:%s, DB:%s: %s', DB_IP, DB_USER, DB_PASSWORD, DB_NAME, error)
+	try:
+		DEVICE_ID = str(deviceID)
+		query = """INSERT INTO OBT (IMEI, VEHICLE_LICENSE, DEVICE_ID, VERSION_ID, ALARM_RATE,COMS_MODULE,CONFIGURATION_ID,CONNECTED,MAX_INVALID_TRACKING_SPEED,PRIORITY,REPLICATED_SERVER_ID,GSM_OPERATOR_ID,ID_CARTOGRAPHY_LAYER,ID_TIME_ZONE,INIT_CONFIG,STAND_BY_RATE,HOST,LOGGER,TYPE_SPECIAL_OBT) VALUES (xxx,xxx,yyy,'11','','127.0.0.1',0,0,500,0,0,0,0,1,'','','',0,12)"""
+		queryOBT = query.replace('xxx', vehicleLicense).replace('yyy', DEVICE_ID)
+		query = """INSERT INTO HAS (FLEET_ID,VEHICLE_LICENSE,DEVICE_ID) VALUES (533,xxx,yyy)"""
+		queryHAS = query.replace('xxx', vehicleLicense).replace('yyy', DEVICE_ID)
+		cursor = dbConnection.cursor()
+		cursor.execute(queryOBT)
+		dbConnection.commit()
+		logger.info('OBT info saved at database for deviceID %s', deviceID)
+		cursor.execute(queryHAS)
+		dbConnection.commit()
+		logger.info('HAS info saved at database for deviceID %s', deviceID)
+		cursor.close
+		dbConnection.close()
+	except Exception, error:
+		logger.error('Error executing query: %s', error)
+		
+		
+	
 
-def send2kcs(message):
-    global socketKCS
-
-    try:
-        socketKCS.send(message)
-        logger.info("[->] Sent to KCS")
-        return True
-    except Exception, error:
-        logger.error('Error sending data to KCS: %s',error)
-        logger.info('Trying reconnection to KCS')
-        try:
-            socketKCS = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            socketKCS.settimeout(0.1)
-            socketKCS.connect((KCS_HOST, int(KCS_PORT)))
-            socketKCS.settimeout(0.05)
-            socketKCS.send(message)
-            logger.info("[->] Sent to KCS")
-            return True
-        except:
-            logger.info('Failed reconnection to KCS')
-            return False
+def getvehicleLicense(body):
+	try:
+		vehicleLicense = body.split(',')[0]
+		return vehicleLicense
+	except Exception, error:
+		logger.error('error parsing message: %s' % error)	
 
 
-def calculateSleep():
-    global SLEEP_TIME
-    global DEFAULT_SLEEP_TIME
-    url = "http://" + RABBITMQ_HOST + ":" + RABBITMQ_PORT + "/api/queues/%2F/" + QUEUE_NAME 
-    h = httplib2.Http(".cache")
-    h.add_credentials(RABBITMQ_ADMIN_USERNAME, RABBITMQ_ADMIN_PASSWORD) 
+def callback(ch, method, properties, body):
+    logger.info("Message read from QUEUE: %s" % body)
+    #antes de enviar al KCS comprobamos si existe el barco en BD
+    vehicleLicense = getvehicleLicense(body)
+    logger.info('Checking if boat %s is at database...', vehicleLicense)
     
-    # Cada 20 segundos se lee el tamaño de la cola para recalcular el SLEEP_TIME
-    while True:
-        resp, content = h.request(url)
-        if (resp.status == 200):
-            content_json = json.loads(content)
-            if (content_json['messages'] == 0):
-                SLEEP_TIME = DEFAULT_SLEEP_TIME
-            else:
-                SLEEP_TIME = 15/content_json['messages']
-            logger.info ("Mensajes en cola=" + str(content_json['messages']) + " - SLEEP TIME=" + str(SLEEP_TIME))
-        time.sleep(20)
-
-def proccessQueue():
-    global rabbitMQconnection
-    while True:
+    if (checkBoat(vehicleLicense) == '0'):
+		logger.info('Boat is not at database.')
+		# creamos el dispositivo
+		deviceID = addBoat(vehicleLicense)
+		logger.info('Boat saved at database with DEVICE_ID %s', deviceID)
+		addComplementary(vehicleLicense, deviceID)
+    else:
+		logger.info('Boat %s found at database', vehicleLicense)
+		
+    try:
+	socketKCS = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	socketKCS.connect((KCS_HOST, int(KCS_PORT)))
+        socketKCS.send(body + '\r\n')
+        logger.info ("Sent to KCS: %s " % body)
+	ch.basic_ack(delivery_tag = method.delivery_tag)
+	socketKCS.close()
+        time.sleep(DEFAULT_SLEEP_TIME)
+    except Exception, error:
+        logger.error('Error sending data: %s', error)
         try:
-            if (rabbitMQconnection == None):
-                rabbitMQconnection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
+			connectedKCS = False
+			socketKCS.close()
+	except:
+		pass
+	while connectedKCS==False:
+		try:
+			logger.info('Trying reconnection to KCS...')
+			socketKCS = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			socketKCS.connect((KCS_HOST, int(KCS_PORT)))
+			socketKCS.send(body + '\r\n')
+			logger.info ("Sent to KCS: %s " % body)
+			ch.basic_ack(delivery_tag = method.delivery_tag)
+			connectedKCS = True
+			socketKCS.close()
+		except Exception, error:
+			logger.info('Reconnection to KCS failed....waiting %d seconds to retry.' , connectionRetry)
+			try:
+				socketKCS.close()
+			except:
+				pass
+			time.sleep(connectionRetry)
+	#time.sleep(DEFAULT_SLEEP_TIME)
 
-            channel = rabbitMQconnection.channel()
-            channel.queue_declare(queue=QUEUE_NAME, durable=True)
-            logger.info(' [*] Waiting for messages. To exit press CTRL+C')
-
-            def callback(ch, method, properties, body):
-                logger.debug(" [x] Received %r" % body)
-        
-                # Bucle de envio del mensaje al KCS
-                resultKCSK = False
-                while resultKCSK != True:
-                    resultKCSK = send2kcs(body)
-                    time.sleep(0.2)
-                
-                # Confirma la lectura del mensaje    
-                ch.basic_ack(delivery_tag = method.delivery_tag)
-                
-                # Espera antes de leer el siguiente mensaje
-                #print "Espera despues de leer: " + str(SLEEP_TIME)
-                time.sleep(SLEEP_TIME)
-
-            channel.basic_qos(prefetch_count=1)
-            channel.basic_consume(callback,
-                              queue=QUEUE_NAME)
-
-            channel.start_consuming()
-
-        except Exception, error:
-            logger.error('Error connecting to rabbitMQ: %s', error)
-            rabbitMQconnection = None
-            time.sleep(0.2)
-
-########################################################################
-# Funcion principal
-#
-########################################################################
-
-def main():
-    global SLEEP_TIME
-    rabbitMQconnection = None
-
-    thread1 = Thread(target=calculateSleep)
-    thread1.start()
-
-    thread2 = Thread(target=proccessQueue)
-    thread2.start()
-
-    thread1.join()
-    thread2.join()
-
-    
-
-if __name__ == '__main__':
-    connectKCS()
-    main()
+	
+try:
+	rabbitMQconnection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
+	channel = rabbitMQconnection.channel()
+	channel.basic_consume(callback, queue=QUEUE_NAME)
+	connectedQUEUE = True
+	logger.info('Connected at QUEUE!!!')
+	channel.start_consuming()
+except Exception, error:
+	connectedQUEUE = False
+	logger.error('Problem with RabbitMQ connection: %s' % error)
+	while connectedQUEUE == False:
+		try:
+			rabbitMQconnection.close()
+		except:
+			pass
+		try:
+			logger.info('Trying reconnection to QUEUE...')
+			rabbitMQconnection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
+			channel = rabbitMQconnection.channel()
+			channel.basic_consume(callback, queue=QUEUE_NAME)
+			connectedQUEUE = True
+			logger.info('Connected at QUEUE again!!!')
+			channel.start_consuming()
+		except Exception, error:
+			logger.info('Reconnection to QUEUE failed....waiting %d seconds to retry.' , connectionRetry)
+			time.sleep(connectionRetry)
+			
